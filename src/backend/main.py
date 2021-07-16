09a450deee1cf5429ba256def4a59cf44a068c36
KDawgthe2nd <kari.hlynsson@gmail.com>
2021-07-16 21:16:09 +0000

    fixing live plotting

--- src/backend/main.py
+++ src/backend/main.py
@@ -13,8 +13,9 @@ def main(init_genotype_distribution=0.2, difficulty=1.2, n=100, percent=0.2, dif
     count = 0
 
     # csv data structure and initialization
-    data = {"time_elapsed": [], "population_size": [], "population_fitness": []}
-    df = pd.DataFrame(data)
+    population_data = {"time_elapsed": [],
+                       "population_size": [], "population_fitness": []}
+    df_population = pd.DataFrame(population_data)
 
     while True:
         # cycle the population
@@ -22,21 +23,28 @@ def main(init_genotype_distribution=0.2, difficulty=1.2, n=100, percent=0.2, dif
 
         # calculate various population parameters
         population_size = len(population._members)
-        population_fitness = population.mean_fitness()
+        population_avg_fitness = population.mean_fitness()
 
-        next_row = {"time_elapsed": count, "population_size": population_size,
-                    "population_fitness": population_fitness}
+        population_data_next = {"time_elapsed": count, "population_size": population_size,
+                                "population_avg_fitness": population_avg_fitness}
+
+        population_fitness = []
+        for i in population._members:
+            population_fitness.append(i._fitness)
+
+        df_fitness = pd.DataFrame(population_fitness, columns=['fitness'])
 
         # write out population size for line plot
-        df = df.append(next_row, ignore_index=True)
-        df.to_csv('../csv/population_data.csv', index=False)
+        with open('../csv/population_data.csv', 'w') as output1:
+            df_population = df_population.append(
+                population_data_next, ignore_index=True)
+            df_population.to_csv(output1, index=False, sep=',')
+        output1.close()
 
         # write out fitness distribution for kde plot
-        with open('../csv/fitness_distribution.csv', 'w') as output:
-            csv_write = csv.writer(output)
-            for i in population._members:
-                next_row = {i._fitness}
-                csv_write.writerow(next_row)
+        with open('../csv/fitness_distribution.csv', 'w') as output2:
+            df_fitness.to_csv(output2, index=False, sep=',')
+        output2.close()
 
         count += 1
 
--- src/frontend/visuals.py
+++ src/frontend/visuals.py
@@ -1,55 +1,72 @@
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gs
+import matplotlib.animation as animation
+
 import seaborn as sns
 import pandas as pd
 import numpy as np
 
 from scipy.stats import kde
 
-
-# read in csv output files of backend
-# ...................................
-
-# read in population size over time elapsed
-
-
-df_size = pd.read_csv('../csv/population_data.csv', sep=',')
-
-time_elapsed = df_size['time_elapsed']
-population_size = df_size['population_size']
-average_pop_fitness = df_size['population_fitness']
-
-# read in population fitness distribution
-df_distribution = pd.read_csv(
-    '../csv/fitness_distribution.csv', sep=',', names=(['fitness']))
-
-population_fitness = df_distribution['fitness']
-
-average_fitness = np.mean(population_fitness)
-
-# visualization
+# global
 fig = plt.figure(1)
-gs = gs.GridSpec(nrows=2, ncols=2)
+gridspec = gs.GridSpec(nrows=2, ncols=2)
+
+# animate function
 
 
-# population size
-ax1 = plt.subplot(gs[0, 0:2])
-ax1.plot(time_elapsed, population_size, color="#6889ed")
-ax1.set_xlabel('Time elapsed')
-ax1.set_ylabel('Population size')
+def animate(i):
 
-# fitness distribution
-ax2 = plt.subplot(gs[1, 0])
-sns.kdeplot(population_fitness)
-ax2.set_xlabel('Fitness value')
-ax2.set_ylabel('Density estimate')
+    # read in csv output files of backend
+    # ...................................
 
-# avg fitness as function of time elapsed
-ax3 = plt.subplot(gs[1, 1])
-ax3.plot(time_elapsed, average_pop_fitness)
-ax3.set_xlabel('Time elapsed')
-ax3.set_ylabel('Average fitness')
+    # read in population size over time elapsed
+    with open('../csv/population_data.csv') as input:
+        df_size = pd.read_csv(input, sep=',', encoding='utf-8', header=1)
 
-plt.suptitle('Evolutionary Algorithm')
+    input.close()
+
+    time_elapsed = df_size['time_elapsed']
+    population_size = df_size['population_size']
+    average_pop_fitness = df_size['population_fitness']
+
+    # read in population fitness distribution
+    with open('../csv/fitness_distribution.csv') as input:
+        df_distribution = pd.read_csv(
+            input, sep=',', names=(['fitness']), encoding='utf-8')
+
+    input.close()
+
+    population_fitness = df_distribution['fitness']
+
+    # population size
+    ax1 = plt.subplot(gridspec[0, 0:2])
+    ax1.plot(time_elapsed, population_size, color="#6889ed")
+    ax1.set_xlabel('Time elapsed')
+    ax1.set_ylabel('Population size')
+
+    # fitness distribution
+    ax2 = plt.subplot(gridspec[1, 0])
+    print(population_fitness)
+    sns.kdeplot(population_fitness)
+    ax2.set_xlabel('Fitness value')
+    ax2.set_ylabel('Density estimate')
+
+    # avg fitness as function of time elapsed
+    ax3 = plt.subplot(gridspec[1, 1])
+    ax3.plot(time_elapsed, average_pop_fitness)
+    ax3.set_xlabel('Time elapsed')
+    ax3.set_ylabel('Average fitness')
+
+    plt.suptitle('Evolutionary Algorithm')
+    plt.pause(10)
+
+    ax1.cla()
+    ax2.cla()
+    ax3.cla()
+
+
+anim = animation.FuncAnimation(fig, animate, interval=250)
+
 plt.tight_layout()
 plt.show()
