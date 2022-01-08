212be9007953bde28a5f5eb768170e9e6c6ced79
Kári <kari.hlynsson@gmail.com>
2022-01-08 19:20:37 +0000

    first commit

---
+++ src/simevo.py
@@ -0,0 +1,16 @@
+import pygame
+
+
+# Denotes dimensions of
+# surface (X = nx, Y = ny)
+nx = 200
+ny = 200
+
+# Initial population
+pop_init = 100
+
+# Base genetic component
+gene_length = 18
+base_gene = gene_length * 'A'
+
+print(base_gene)
