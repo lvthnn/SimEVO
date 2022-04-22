e4fc6cddcba2ce5ac3c6d27be11cf2b0a446ac6c
Kári <kari@Karis-MacBook-Air-2.local>
2022-04-22 19:52:19 +0000

    Added new figure and expanded on probabilistic section

--- src/runtime/numpy_analysis.py
+++ src/runtime/numpy_analysis.py
@@ -3,19 +3,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 if __name__ == '__main__':
-    data = pd.read_csv('data.csv', index_col = False)
-    print(data)
+    data = pd.read_csv('data.csv', index_col=False)
 
     fig = plt.figure()
-    ax = fig.add_subplot(projection = '3d')
+    ax = fig.add_subplot(projection='3d')
 
     ax.plot_trisurf(data["n"], data["C"], data["max_load"])
     ax.set_xlabel('Population Size')
     ax.set_ylabel('Number of Chunks')
     ax.set_zlabel('Maximum Chunk Load')
-    plt.show()
-
     plt.savefig('mcl_simulation.png', bbox_inches ='tight')
+    plt.show()
 
 
 
